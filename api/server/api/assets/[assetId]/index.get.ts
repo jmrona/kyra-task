8b79a5311a22543aada3d4b03030ec9d279dd5f7
jmrona <jmrona@users.noreply.github.com>
2025-11-25 14:45:35 +0000

    Local changes

---
+++ api/server/api/assets/[assetId]/index.get.ts
@@ -0,0 +1,21 @@
+export default defineEventHandler(async (event) => {
+  const assetId = getRouterParam(event, "assetId");
+  if (!assetId) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: "Asset ID is required",
+    });
+  }
+
+  const store = useStorage("memory");
+  const assets = await store.getItem<any[]>("assets");
+
+  const asset = assets?.find((asset) => asset.id === Number(assetId));
+  if (!asset) {
+    throw createError({ statusCode: 404, statusMessage: "Asset not found" });
+  }
+
+  return (
+    asset ?? {} 
+  );
+});
